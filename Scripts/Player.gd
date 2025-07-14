b2621130af9e32583d142d5b14645b4159b7a0b8
Jose Gonzalez <gonzaljo0113@gmail.com>
2025-07-13 22:02:48 -0700

    Added Run functionality
    - Added run functionality
    - Organized our project files
    - Started work on crouch functionality

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,9 +2,27 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -350.0
-var RunSpeed = 100.0
+var RunSpeedincrease = 2.0
+var CrouchSpeeddecrease = 0.5
 
 @onready var player_sprite: AnimatedSprite2D = $"Player Sprite"
+@onready var player_collision: CollisionShape2D = $"Player Collision"
+@onready var player_sprite_crouch: AnimatedSprite2D = $"Player Sprite Crouch"
+@onready var player_collision_crouch: CollisionShape2D = $"Player Collision Crouch"
+
+func enable_crouch_player():
+	player_sprite.hide()
+	player_sprite_crouch.show()
+	player_sprite_crouch.play("Crouch")
+	player_collision.hide()
+	player_collision_crouch.show()
+
+func disable_crouch_player():
+	player_sprite_crouch.hide()
+	player_sprite.show()
+	player_sprite_crouch.play("Stand")
+	player_collision_crouch.hide()
+	player_collision.show()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -25,22 +43,24 @@ func _physics_process(delta: float) -> void:
 	elif direction < 0:
 			player_sprite.flip_h = true
 	
+	
 	#Play Animations
 	if is_on_floor():
 		if direction == 0:
 			player_sprite.play("Idle")
-		elif direction != 0:
-			player_sprite.play("Walk")
 		elif direction != 0 and Input.is_action_pressed("Ability_1"):
 			player_sprite.play("Run")
-			print("Running")
+		elif direction != 0 and Input.is_action_pressed("Action_1"):
+			player_sprite_crouch.play("Crouch Walk")
+		elif direction != 0:
+			player_sprite.play("Walk")
 	else:
 		player_sprite.play("Jump")
 	
-	if direction:
+	if direction and Input.is_action_pressed("Ability_1"):
+		velocity.x = direction * SPEED * RunSpeedincrease
+	elif direction:
 		velocity.x = direction * SPEED
-	elif direction and Input.is_action_pressed("Ability_1"):
-		velocity.x = direction * SPEED + RunSpeed
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
