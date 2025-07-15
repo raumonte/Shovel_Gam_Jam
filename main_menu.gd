d8d1e26e06c9b6ba49eb9078674d038f9f0df74f
Kyto14 <kytollc14@gmail.com>
2025-07-14 23:13:18 -0700

    Added "Play" functionality
    - Updated Reference Resolution to 1920x1080, changed from 1152x648
    - Added 2D camea to Main Menu scene
    - Started from scratch
    - Added Play, Options, Exit
    - Added Title
    - Created New Header Large Theme
    - Set play button as Granola Bar
    - Added Quit Button functionality
    - Added Play Button functionality

---
+++ main_menu.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/DevLevel.tscn")
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
