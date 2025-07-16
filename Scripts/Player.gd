369b6b551957bd553c3bbb403ade3d1bdfe82c7e
Jose Gonzalez <gonzaljo0113@gmail.com>
2025-07-15 22:27:52 -0700

    Beetle Movement Implemented
    - Continued work on Stamina
    - Beetle now moves back and forth a set distance
    - Added Music to the DevLevel Scene

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,37 +5,12 @@ const JUMP_VELOCITY = -350.0
 var RunSpeedincrease = 2.0
 var CrouchSpeeddecrease = 0.5
 var PlayerHealth = 100
-var StaminaTimer = 0
+var Stamina = 5
 
 @onready var player_sprite: AnimatedSprite2D = $"Player Sprite"
 @onready var player_collision: CollisionShape2D = $"Player Collision"
-@onready var player_sprite_crouch: AnimatedSprite2D = $"Player Sprite Crouch"
-@onready var player_collision_crouch: CollisionShape2D = $"Player Collision Crouch"
 @onready var timer: Timer = $Timer
 
-func enable_crouch_player():
-	player_sprite.hide()
-	player_sprite_crouch.show()
-	player_sprite_crouch.play("Crouch")
-	player_collision.hide()
-	player_collision_crouch.show()
-
-func disable_crouch_player():
-	player_sprite_crouch.hide()
-	player_sprite.show()
-	player_sprite_crouch.play("Stand")
-	player_collision_crouch.hide()
-	player_collision.show()
-
-func StaminaDrain():
-	if Input.is_action_just_pressed("Ability_1"):
-		StaminaTimer = timer.time_left
-		print(timer.time_left)
-		timer.start()
-	elif Input.is_action_just_released("Ability_1"):
-		timer.stop()
-		StaminaTimer = timer.time_left
-		print(timer.time_left)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -50,6 +25,15 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	
+	var StaminaUsed = timer.time_left
+	
+	if Input.is_action_just_pressed("Ability_1"):
+		timer.start(5)
+	elif Input.is_action_just_released("Ability_1"):
+		timer.stop()
+	print(StaminaUsed)
+	Stamina = StaminaUsed
+		
 	#Flip the sprite
 	if direction > 0:
 		player_sprite.flip_h = false
@@ -61,16 +45,14 @@ func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		if direction == 0:
 			player_sprite.play("Idle")
-		elif direction != 0 and Input.is_action_pressed("Ability_1") and StaminaTimer !=0:
+		elif direction != 0 and Input.is_action_pressed("Ability_1") and Stamina !=0:
 			player_sprite.play("Run")
-		elif direction != 0 and Input.is_action_pressed("Action_1"):
-			player_sprite_crouch.play("Crouch Walk")
 		elif direction != 0:
 			player_sprite.play("Walk")
 	else:
 		player_sprite.play("Jump")
 	
-	if direction and Input.is_action_pressed("Ability_1") and StaminaTimer != 0:
+	if direction and Input.is_action_pressed("Ability_1") and Stamina != 0:
 		velocity.x = direction * SPEED * RunSpeedincrease
 	elif direction:
 		velocity.x = direction * SPEED
--- beetle.gd
+++ beetle.gd
@@ -4,16 +4,20 @@ extends CharacterBody2D
 @onready var aggro: Sprite2D = $Aggro
 @onready var beetle_collision: CollisionShape2D = $"Beetle Collision"
 @onready var hitbox: Area2D = $Hitbox
-@onready var move_left_timer: Timer = $"Move Left Timer"
-@onready var move_right_timer: Timer = $"Move Right Timer"
+
+const Speed = 100
+var OriginPoint = get_position()
+var direction = 1
 
 
-
-
-func _on_move_right_timer_timeout() -> void:
-	move_left_timer.start()
-	global_position + Vector2(20,0)
+func _process(delta: float) -> void:
+	var CurrentPosition = get_position_delta()
 	
-func _on_move_left_timer_timeout() -> void:
-	move_right_timer.start()
-	global_position + Vector2(-20,0)
+	if CurrentPosition > (OriginPoint + Vector2(200,0)):
+		direction = -1
+		neutral.flip_h = true
+	if CurrentPosition < (OriginPoint - Vector2(200,0)):
+		direction = 1
+		neutral.flip_h = false
+	
+	position.x += direction * Speed * delta
