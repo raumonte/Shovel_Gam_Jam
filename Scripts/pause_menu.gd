2f66b401254c585861c610f43a89fa7b6f06f140
Kyto14 <kytollc14@gmail.com>
2025-07-17 19:00:18 -0700

    Added Pause Menu
    - Added Pause Menu
    - functional but very buggy

---
+++ Scripts/pause_menu.gd
@@ -0,0 +1,30 @@
+extends Control
+
+func _ready():
+	$AnimationPlayer.play("RESET")
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play("blur")
+
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play_backwards("blur")
+
+func _unhandled_input(_delta):
+	testEsc()
+
+func testEsc():
+	if Input.is_action_just_pressed("pause") and get_tree().paused == false:
+		pause()
+	elif Input.is_action_just_pressed("pause") and get_tree().paused == true:
+		resume()
+
+func _on_resume_pressed():
+	resume()
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _on_options_pressed():
+	pass # Replace with function body
