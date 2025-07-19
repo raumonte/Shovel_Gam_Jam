1abeff40865f89fd624a7654decf0aafd67ad4e6
Kyto14 <kytollc14@gmail.com>
2025-07-18 22:13:37 -0700

    Music Loop Script
    Added music loop scene

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,5 +1,5 @@
 extends CanvasLayer
-
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $Control/MusicStreamPlayer/AudioStreamPlayer2D
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/DevLevel.tscn")
@@ -11,3 +11,15 @@ func _on_options_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func update_Musictracker():
+	Global.Musictracker = main_menu_background_music.get_playback_position()
+func _ready() -> void:
+	if Global.Musictracker == 0:
+		main_menu_background_music.play()
+	else: main_menu_background_music.play(Global.Musictracker)
+
+#Sends player to the game scene
+func _on_play_pressed() -> void:
+	update_MusicTracker()
+	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
