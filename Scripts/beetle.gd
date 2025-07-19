2be79ba5526ca1c2c62c110c25b5c140dc25d7e0
Jose Gonzalez <gonzaljo0113@gmail.com>
2025-07-18 22:05:33 -0700

    Trying to get an attack to work
    - It ain't working yet
    - got the hitbox and raycast to flip though!

--- Scripts/beetle.gd
+++ Scripts/beetle.gd
@@ -6,23 +6,39 @@ extends CharacterBody2D
 @onready var hitbox: Area2D = $Hitbox
 @onready var label: Label = $Label
 @onready var scene_manager: Node = %SceneManager
+@onready var node_2d: Node2D = $Node2D
+@onready var ray_cast_2d: RayCast2D = $Node2D/RayCast2D
 
-
-
-const Speed = 100
+var Speed = 100
 var direction = 1
-
+var is_Attacking = false
 
 func _process(delta: float) -> void:
 	var CurrentPosition = get_position_delta()
 	
-	if CurrentPosition > (scene_manager.BeetleOriginPoint + Vector2(100,0)):
-		direction = -1
-		neutral.flip_h = true
-	if CurrentPosition < (scene_manager.BeetleOriginPoint - Vector2(100,0)):
-		direction = 1
-		neutral.flip_h = false
+	if ray_cast_2d.is_colliding() == true:
+		$"Reaction Timer".start()
+		is_Attacking = true
+		ray_cast_2d.set_collision_mask(3)
+		print(ray_cast_2d.collision_mask)
+	elif $"Reaction Timer".is_stopped() and is_Attacking == true:
+		Speed + 300
+		$"Attack Timer".start()
+		$"Attack Cooldown".start()
+	elif $"Attack Timer".is_stopped() and is_Attacking == true:
+		is_Attacking = false
+		Speed - 300
+	elif $"Attack Cooldown".is_stopped() and is_Attacking == false:
+		ray_cast_2d.enabled = true
 		
-		label.text = "Origin Point: " + str(scene_manager.BeetleOriginPoint)
+	if CurrentPosition > (scene_manager.BeetleOriginPoint + Vector2(100,0)) and is_Attacking == false:
+		direction = -1
+	elif CurrentPosition < (scene_manager.BeetleOriginPoint - Vector2(100,0)) and is_Attacking == false:
+		direction = 1
+		
+	node_2d.scale.x = direction
+	
+	label.text = "Origin Point: " + str(scene_manager.BeetleOriginPoint)
+	$Label2.text = "Am I attacking:" + str(is_Attacking)
 	
 	position.x += direction * Speed * delta
